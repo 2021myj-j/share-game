2a60a099f4dd9d499f1932f7bb19da24d1163e90
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-08-15 00:16:10 +0900

    format code

--- main.py
+++ main.py
@@ -1,29 +1,6 @@
-# import datetime
 from typing import List, Tuple, Optional
 
 from game.game import App
-# from utils import update_func  # , confing
-"""
-def add_instance_var(self):
-    self.frame_counter = 0
-
-
-def input_command(self):
-
-    global command_list
-
-    if self.counttime.microseconds < 975000:
-        return
-
-    if not command_list:
-        command_list = get_command()
-
-    self.player.a_pressed, self.player.d_pressed = command_list.pop(0)
-
-
-def get_command():
-    return [(True, False) for i in range(6)] + [(False, True) for i in range(6)]
-"""
 
 
 class App(App):
@@ -61,30 +38,5 @@ class App(App):
 
 
 if __name__ == "__main__":
-    """
-    command_list_per_s: List[Tuple[bool, bool]]
-    command_list_per_s: List[Tuple[a_pressed, b_pressed]]
-    """
-    # command_list: Optional[List[Tuple[bool, bool]]] = None
-
-    # command_list = [(False, True) for i in range(3)]
-
-    # execution_before_update = [input_command]
-    # execution_after_update = []
-
-    # execution_before_init = [add_instance_var]
-    # execution_after_init = []
-
-    # App.update = update_func(
-    #     App.update,
-    #     before=execution_before_update,
-    #     after=execution_after_update
-    # )  # yapf: disable
-
-    # App.__init__ = update_func(
-    #     App.__init__,
-    #     before=execution_before_init,
-    #     after=execution_after_init
-    # )  # yapf: disable
 
     App()
